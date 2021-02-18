ddf10236ea50700fb80f505a780d3f462ef03cab
AkinchanKushwaha <akinchandroger@gmail.com>
2021-02-18 22:56:07 +0530

    Build the layout and add chart Interactions

--- app/src/main/java/com/example/covidtracker/MainActivity.kt
+++ app/src/main/java/com/example/covidtracker/MainActivity.kt
@@ -3,25 +3,39 @@ package com.example.covidtracker
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.core.content.ContextCompat
+import com.example.covidtracker.databinding.ActivityMainBinding
 import com.google.gson.GsonBuilder
+import com.robinhood.ticker.TickerUtils
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.Tag
+import java.text.NumberFormat
+import java.text.SimpleDateFormat
+import java.util.*
 
 private const val BASE_URL = "https://covidtracking.com/api/v1/"
 private const val TAG = "MainActivity"
+private const val ALL_STATES : String = "All (Nationwide)"
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var currentlyShownData: List<CovidData>
+    private lateinit var adapter: CovidSparkAdapter
+    private lateinit var binding : ActivityMainBinding
+
 
     private lateinit var perStateDailyData: Map<String, List<CovidData>>
     private lateinit var nationalDailyData: List<CovidData>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
         val gson = GsonBuilder().setDateFormat("yyyy-MM-dd'T'HH:mm:ss").create()
         val retrofit = Retrofit.Builder()
@@ -40,8 +54,10 @@ class MainActivity : AppCompatActivity() {
                     Log.w(TAG, "Did not receive a valid response body")
                     return
                 }
+                setupEventListeners()
                 nationalDailyData = nationalData.reversed()
                 Log.i(TAG, "Update graph with national data")
+                updateDisplayWithData(nationalDailyData)
             }
 
             override fun onFailure(call: Call<List<CovidData>>, t: Throwable) {
@@ -60,11 +76,99 @@ class MainActivity : AppCompatActivity() {
                 }
                 perStateDailyData = statesData.reversed().groupBy { it.state }
                 Log.i(TAG, "Update graph with state names")
+                updateSpinnerWithStateData(perStateDailyData.keys)
             }
 
             override fun onFailure(call: Call<List<CovidData>>, t: Throwable) {
                 Log.e(TAG,"onFailure $t")
             }
         })
+    }
+
+    private fun updateSpinnerWithStateData(stateNames: Set<String>) {
+        val stateAbbreviationList = stateNames.toMutableList()
+        stateAbbreviationList.sort()
+        stateAbbreviationList.add(0,ALL_STATES)
+
+        // Add state list as data source for the spinner
+        binding.spinnerSelect.attachDataSource(stateAbbreviationList)
+        binding.spinnerSelect.setOnSpinnerItemSelectedListener { parent, _, position, _ ->
+            val selectedState = parent.getItemAtPosition(position) as String
+            val selectedData = perStateDailyData[selectedState] ?: nationalDailyData
+            updateDisplayWithData(selectedData)
+        }
+    }
+
+    private fun setupEventListeners() {
+
+        binding.tickerView.setCharacterLists(TickerUtils.provideNumberList())
+
+        binding.sparkView.isScrubEnabled = true
+        binding.sparkView.setScrubListener {itemData ->
+            if(itemData is CovidData){
+                updateInfoForDate(itemData)
+            }
+        }
+
+        binding.radioGroupTimeSelection.setOnCheckedChangeListener { _, checkedId ->
+            adapter.daysAgo = when(checkedId) {
+                R.id.radioButtonWeek -> TimeScale.WEEK
+                R.id.radioButtonMonth -> TimeScale.MONTH
+                else -> TimeScale.MAX
+            }
+            adapter.notifyDataSetChanged()
+        }
+        binding.radioGroupMetricSelection.setOnCheckedChangeListener { _, checkedId ->
+            when(checkedId){
+                R.id.radioButtonPositive ->updateDisplayMetric(Metric.POSITIVE)
+                R.id.radioButtonNegative ->updateDisplayMetric(Metric.NEGATIVE)
+                R.id.radioButtonDeath  ->updateDisplayMetric(Metric.DEATH)
+            }
+        }
+    }
+
+    private fun updateDisplayMetric(metric: Metric) {
+        // update the color of the chart
+
+        val colorRes = when(metric){
+            Metric.NEGATIVE -> R.color.colorNegative
+            Metric.DEATH -> R.color.colorDeath
+            Metric.POSITIVE -> R.color.colorPositive
+        }
+        val colorInt: Int = ContextCompat.getColor(this,colorRes)
+        binding.sparkView.lineColor = colorInt
+        binding.tickerView.setTextColor(colorInt)
+        // update the metric on the adapter
+        adapter.metric = metric
+        adapter.notifyDataSetChanged()
+
+        // reset number and date shown in the bottom text view
+        updateInfoForDate(currentlyShownData.last())
+    }
+
+    private fun updateDisplayWithData(dailyData: List<CovidData>) {
+
+        currentlyShownData = dailyData
+
+        adapter = CovidSparkAdapter(dailyData)
+        binding.sparkView.adapter = adapter
+
+        binding.radioButtonPositive.isChecked = true
+        binding.radioButtonMax.isChecked = true
+        updateDisplayMetric(Metric.POSITIVE)
+
+    }
+
+    private fun updateInfoForDate(covidData: CovidData) {
+        val numCases = when(adapter.metric){
+            Metric.NEGATIVE -> covidData.negativeIncrease
+            Metric.POSITIVE -> covidData.positiveIncrease
+            Metric.DEATH -> covidData.deathIncrease
+        }
+
+        binding.tickerView.text = NumberFormat.getInstance().format(numCases)
+        val outputDateFormat = SimpleDateFormat("MMM dd, yyyy", Locale.US)
+        binding.tvDateLabel.text = outputDateFormat.format(covidData.dateChecked)
+
     }
 }
